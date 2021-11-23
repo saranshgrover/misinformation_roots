8ca76241d74e66b57d427b04ee501080f6ede233
Saransh Grover <contact@saranshgrover.com>
2021-11-22 21:55:07 -0500

    abcd

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
 import { ColorModeScript } from '@chakra-ui/react';
 import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './components/App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
+import FirebaseProvider from './store/firerbase'
+
 
 ReactDOM.render(
   <StrictMode>
-    <ColorModeScript />
-    <App />
+    <ColorModeScript initialColorMode="light" />
+    <FirebaseProvider>
+      <App />
+    </FirebaseProvider>
   </StrictMode>,
   document.getElementById('root')
 );
